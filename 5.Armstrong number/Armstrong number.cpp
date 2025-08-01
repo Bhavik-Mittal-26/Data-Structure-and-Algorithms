3e705fe851eea3ae220b8bfdfcd5210ce453158a
Bhavik Mittal <26bmittal@gmail.com>
2025-08-01 22:20:19 +0530

    updated

---
+++ 5.Armstrong number/Armstrong number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isArmstrong(int n) {
+        int original=n;
+      int sum=0;
+       int lastdigit=0;
+       while(n>0)
+       {
+        lastdigit=n%10;
+        sum=sum+(lastdigit*lastdigit*lastdigit);
+         n= n/10;
+       }
+       return  original==sum;
+
+    }
+};
