334e62e70c18a089c689acd6b3c19a3388c9e184
Bhavik Mittal <26bmittal@gmail.com>
2025-08-17 19:00:12 +0530

    Added Prime number problem

---
+++ 7.Prime number/Prime number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        int count = 0; 
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2)  // prime numbers ke sirf 2 divisors hote hain
+            return true;
+        else
+            return false;
+    }
+};
