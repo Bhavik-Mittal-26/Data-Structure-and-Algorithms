f79fbe9e04427468ee5d26d250cf05531fad6a64
Bhavik Mittal <26bmittal@gmail.com>
2025-08-17 19:16:18 +0530

    Update Prime number.cpp

--- 7.Prime number/Prime number.cpp
+++ 7.Prime number/Prime number.cpp
@@ -1,3 +1,5 @@
+// This approch is for O(n) 
+
 class Solution {
 public:
     bool isPrime(int n) {
@@ -13,3 +15,20 @@ public:
             return false;
     }
 };
+
+//This approach is for O(sqrt n) 
+
+class Solution {
+public:
+    bool isPrime(int n) {
+        if (n <= 1) return false;  
+
+        for (int i = 2; i * i <= n; i++) {  
+            if (n % i == 0) {
+                return false;  
+            }
+        }
+        return true;  
+    }
+};
+
