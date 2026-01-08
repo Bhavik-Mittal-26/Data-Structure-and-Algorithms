74b84f5047ed2b28a97b3e2965556fb4d83f2525
Bhavik Mittal <26bmittal@gmail.com>
2026-01-09 00:54:52 +0530

    added file search insert position leetcode

---
+++ 43.Search insert position - leetcode/code.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return left;   // insert position
+    }
+};
+int main(){
+    Solution s;
+    vector<int>nums = {1,3,5,6};
+    int target = 4;
+    s.searchInsert(nums, 4);
+    cout<<s.searchInsert(nums, 3);
+
+
+}
