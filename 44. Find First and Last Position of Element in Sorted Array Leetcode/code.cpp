b4e9c63a2cb8f9513f176fbc3b0e708c2b4a1cab
Bhavik Mittal <26bmittal@gmail.com>
2026-01-11 22:44:33 +0530

    Modify searchRange function in code.cpp

--- 44. Find First and Last Position of Element in Sorted Array Leetcode/code.cpp
+++ 44. Find First and Last Position of Element in Sorted Array Leetcode/code.cpp
@@ -4,7 +4,6 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans(2, -1);
-
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == target) {
                 if (ans[0] == -1) {
