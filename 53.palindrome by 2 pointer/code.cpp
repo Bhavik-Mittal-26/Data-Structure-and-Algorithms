6345bcc598bf9746139a71dae19ed15341b20058
Bhavik Mittal <26bmittal@gmail.com>
2026-03-23 01:16:15 +0530

    palindrome by 2 pointer

---
+++ 53.palindrome by 2 pointer/code.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+
+        string s = to_string(x);
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    Solution s;
+    int x = 121;
+
+   cout<< s.isPalindrome(x);
+}
