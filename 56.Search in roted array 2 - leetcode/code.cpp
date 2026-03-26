486cfaa462448b1e359e1d0be2618ae655dbfbfb
Bhavik Mittal <26bmittal@gmail.com>
2026-03-26 13:18:58 +0530

    Added Search in rotated arr 2nd

---
+++ 56.Search in roted array 2 - leetcode/code.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        for(int i =0; i<nums.size();i++){
+            if(nums[i]==target){
+                return true;
+            }
+            
+            }
+        
+        return false;
+        
+    }
+};
+int main(){
+Solution s;
+vector<int>nums={3,8,4,7,8};
+int target = 6;
+s.search(nums,target);
+cout<<s.search(nums,target);
+
+    return 0;
+}
