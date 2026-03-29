9611bef857fc32a114834646ba1362e59f4c3b02
Bhavik Mittal <26bmittal@gmail.com>
2026-03-29 11:29:26 +0530

    Added file

---
+++ 70.Deleting node linked list - leetcode/code.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(4);
+    head->next = new ListNode(5);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(9);
+
+    cout << "Original List: ";
+    printList(head);
+
+    Solution obj;
+    obj.deleteNode(head->next);
+
+    cout << "After Deletion: ";
+    printList(head);
+
+    return 0;
+}
