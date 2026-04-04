e6ee8300c36fe030cfc56c268eb00d1e424db2af
Bhavik Mittal <26bmittal@gmail.com>
2026-04-04 23:46:38 +0530

    Refactor ListNode constructor formatting

--- 64.Insertion at head in doubly linked list/code.cpp
+++ 64.Insertion at head in doubly linked list/code.cpp
@@ -5,7 +5,6 @@ public:
     int data;
     ListNode *prev;
     ListNode *next;
-
     ListNode() : data(0), prev(nullptr), next(nullptr) {}
     ListNode(int x) : data(x), prev(nullptr), next(nullptr) {}
     ListNode(int x, ListNode *prev, ListNode *next) : data(x), prev(prev), next(next) {}
