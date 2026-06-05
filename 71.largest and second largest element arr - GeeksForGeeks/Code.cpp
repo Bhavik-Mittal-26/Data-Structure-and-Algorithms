829533d396aa8316c60e9a565059daa829a70a03
Bhavik Mittal <26bmittal@gmail.com>
2026-06-05 23:24:56 +0530

    Refactor code structure for improved readability and maintainability

---
+++ 71.largest and second largest element arr - GeeksForGeeks/Code.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] != arr[0]) {
+            cout << arr[0] << " " << arr[i];
+            return 0;
+        }
+    }
+
+    cout << -1;
+
+    return 0;
+}
