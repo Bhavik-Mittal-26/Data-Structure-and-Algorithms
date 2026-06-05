3cec626d842e78f53315d1b5a418080579c959b5
Bhavik Mittal <26bmittal@gmail.com>
2026-06-05 22:50:43 +0530

    Refactor code structure for improved readability and maintainability

---
+++ 69.Reverse a string - Leetcode/Code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+
+    cout << "Enter string: ";
+    cin >> s;
+
+    reverse(s.begin(), s.end());
+
+    cout << s;
+
+    return 0;
+}
