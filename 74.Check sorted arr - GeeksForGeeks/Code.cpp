13fbb0af13c2e6933a63867b27c16e7be2768ea3
Bhavik Mittal <26bmittal@gmail.com>
2026-06-13 22:52:21 +0530

    Refactor code structure for improved readability and maintainability

---
+++ 74.Check sorted arr - GeeksForGeeks/Code.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+        int n ;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+     for (int i =0 ; i<n-1;i++){
+         if(arr[i]>arr[i+1]){
+             cout<<"false";
+         }
+        else{cout<<"true";}
+        return 0;
+     }   
+    }
