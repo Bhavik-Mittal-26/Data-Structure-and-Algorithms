b6d800b3e8618f603df529f57979b05df6b92e41
Bhavik Mittal <26bmittal@gmail.com>
2026-07-01 23:35:40 +0530

    kedane algo

---
+++ 79.Maximum Subarr sum - Leetcode/Code.cpp
@@ -0,0 +1,25 @@
+//kedane algorith 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int maxSum = arr[0];
+    int currSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
+    }
+
+    cout << "Maximum Subarray Sum = " << maxSum;
+
+    return 0;
+}
