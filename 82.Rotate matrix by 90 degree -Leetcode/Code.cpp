b84afd841d16f6baaa9d172374927ea36be4414f
Bhavik Mittal <26bmittal@gmail.com>
2026-07-04 16:09:20 +0530

    Rotate matrix

---
+++ 82.Rotate matrix by 90 degree -Leetcode/Code.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // below reverse is important for getting final answer 
+        for(int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    Solution obj;
+    obj.rotate(matrix);
+
+    cout << "Rotated Matrix:\n";
+    for (auto &row : matrix) {
+        for (auto &val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
