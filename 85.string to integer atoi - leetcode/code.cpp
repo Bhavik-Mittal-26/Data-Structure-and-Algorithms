3f3b26f18abcff03cd1b8b2e825d3c0d76fcdb6e
Bhavik Mittal <26bmittal@gmail.com>
2026-07-05 23:59:40 +0530

    Add implementation of string to integer (atoi) conversion

---
+++ 85.string to integer atoi - leetcode/code.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        long long ans = 0;
+        int sign = 1;
+
+        // Skip leading spaces
+        while (i < s.size() && s[i] == ' ')
+            i++;
+
+        // Check sign
+        if (i < s.size()) {
+            if (s[i] == '-') {
+                sign = -1;
+                i++;
+            }
+            else if (s[i] == '+') {
+                i++;
+            }
+        }
+
+        // Read digits
+        while (i < s.size()) {
+            if (s[i] >= '0' && s[i] <= '9') {
+                ans = ans * 10 + (s[i] - '0');
+
+                if (sign == 1 && ans > INT_MAX)
+                    return INT_MAX;
+
+                if (sign == -1 && -ans < INT_MIN)
+                    return INT_MIN;
+            }
+            else {
+                break;
+            }
+
+            i++;
+        }
+
+        return sign * ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    string s;
+    getline(cin, s);
+
+    cout << obj.myAtoi(s);
+
+    return 0;
+}
