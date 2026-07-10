f4aed3a48dbbff111f1349992ef3821a045cb93b
Bhavik Mittal <26bmittal@gmail.com>
2026-07-10 23:34:52 +0530

    koko eating banana by binary search - Leetcode

---
+++ 89.Koko eating banana - leetcode/code.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canEat(vector<int>& piles, int h, int k) {
+        long long hours = 0;
+
+        for (int pile : piles)
+            hours += (pile + k - 1) / k;
+
+        return hours <= h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canEat(piles, h, mid))
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+};
+
+int main() {
+    Solution s;
+
+    vector<int> piles = {3, 6, 7, 11};
+    int h = 8;
+
+    int ans = s.minEatingSpeed(piles, h);
+
+    cout << "Minimum Eating Speed = " << ans << endl;
+
+    return 0;
+}
