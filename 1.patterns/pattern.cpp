35073fb6b51998ed32a1a52ae81dc05f0f1ef7be
Bhavik Mittal <26bmittal@gmail.com>
2026-08-04 23:10:41 +0530

    updated file

--- 1.patterns/pattern.cpp
+++ 1.patterns/pattern.cpp
@@ -130,10 +130,150 @@ void print7(int n)
     }
 }
 
+// Pattern 8: Hollow square
+void print8(int n)
+{
+    cout << "\nPattern 8: Hollow Square\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+}
+
+// Pattern 9: Hollow right triangle
+void print9(int n)
+{
+    cout << "\nPattern 9: Hollow Right Triangle\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0 || j == i || i == n - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+}
+
+// Pattern 10: Inverted hollow triangle
+void print10(int n)
+{
+    cout << "\nPattern 10: Inverted Hollow Triangle\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (i == 0 || j == 0 || j == n - i - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+}
+
+// Pattern 11: Diamond
+void print11(int n)
+{
+    cout << "\nPattern 11: Diamond\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+            cout << " ";
+        for (int j = 0; j < 2 * i + 1; j++)
+            cout << "*";
+        cout << endl;
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+            cout << " ";
+        for (int j = 0; j < 2 * i + 1; j++)
+            cout << "*";
+        cout << endl;
+    }
+}
+
+// Pattern 12: Hourglass
+void print12(int n)
+{
+    cout << "\nPattern 12: Hourglass\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+            cout << " ";
+        for (int j = 0; j < n - i; j++)
+            cout << "* ";
+        cout << endl;
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+            cout << " ";
+        for (int j = 0; j < n - i; j++)
+            cout << "* ";
+        cout << endl;
+    }
+}
+
+// Pattern 13: Number pyramid
+void print13(int n)
+{
+    cout << "\nPattern 13: Number Pyramid\n";
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+            cout << " ";
+        for (int j = 1; j <= i; j++)
+            cout << j << " ";
+        cout << endl;
+    }
+}
+
+// Pattern 14: Butterfly pattern
+void print14(int n)
+{
+    cout << "\nPattern 14: Butterfly Pattern\n";
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+            cout << "* ";
+        for (int j = 1; j <= 2 * (n - i); j++)
+            cout << "  ";
+        for (int j = 1; j <= i; j++)
+            cout << "* ";
+        cout << endl;
+    }
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+            cout << "* ";
+        for (int j = 1; j <= 2 * (n - i); j++)
+            cout << "  ";
+        for (int j = 1; j <= i; j++)
+            cout << "* ";
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
+
+    cout << "Pattern Program\n";
     print1(n);
     print2(n);
     print3(n);
@@ -141,5 +281,12 @@ int main()
     print5(n);
     print6(n);
     print7(n);
+    print8(n);
+    print9(n);
+    print10(n);
+    print11(n);
+    print12(n);
+    print13(n);
+    print14(n);
     return 0;
 }
