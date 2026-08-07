f2d86ffa69d08a0fd97d8fae4d651a81f554c1c5
Bhavik Mittal <26bmittal@gmail.com>
2026-08-07 12:26:19 +0530

    Added problem on greedy approach

---
+++ 103.Greedy cookies-leetcode/code.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int l = 0;
+        int r = 0;
+        int count = 0;
+
+        while (l < s.size() && r < g.size()) {
+
+            if (g[r] <= s[l]) {
+                count++;
+                l++;
+                r++;
+            }
+            else {
+                l++;
+            }
+        }
+
+        return count;
+    }
+};
