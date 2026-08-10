929042d3a433de3eef707719f6febf9335bef3ce
Bhavik Mittal <26bmittal@gmail.com>
2026-08-10 14:04:25 +0530

    Stack problem added

--- 110.Valid parenthisis (using stack)- Leetcode/code.cpp
+++ 110.Valid parenthisis (using stack)- Leetcode/code.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
     bool isValid(string s) {
-
         stack<char> st;
 
-        for(int i = 0; i < s.size(); i++) {
+        for (int i = 0; i < s.size(); i++) {
 
-            if(s[i] == '{' || s[i] == '(' || s[i] == '[') {
-
+            // Opening bracket
+            if (s[i] == '{' || s[i] == '(' || s[i] == '[') {
                 st.push(s[i]);
             }
 
-            // Otherwise, current character is a closing bracket
+            // Closing bracket
             else {
-
-                // If stack is empty, there is no opening bracket
-                // to match the current closing bracket
-                if(st.empty()) {
+                if (st.empty()) {
                     return false;
                 }
 
-                // Get the top opening bracket
-                char ch = st.top();
+                if ((s[i] == '}' && st.top() == '{') ||
+                    (s[i] == ']' && st.top() == '[') ||
+                    (s[i] == ')' && st.top() == '(')) {
 
-                
-                if((s[i] == '}' && ch != '{') ||
-                   (s[i] == ')' && ch != '(') ||
-                   (s[i] == ']' && ch != '[')) {
-
-            
+                    st.pop();
+                }
+                else {
                     return false;
                 }
-
-                // Matching pair found, so remove opening bracket
-                st.pop();
             }
         }
 
-        // If stack is empty, every bracket was matched
         return st.empty();
     }
 };
---
+++ 111.Next greater element-Leetcode/code.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        stack<int> st;
+        unordered_map<int, int> mp;
+
+        // Process nums2 from right to left
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+
+            // Remove smaller elements
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            // Find answer
+            if (st.empty()) {
+                mp[nums2[i]] = -1;
+            } else {
+                mp[nums2[i]] = st.top();
+            }
+
+            // Push current element
+            st.push(nums2[i]);
+        }
+
+        // Answer for nums1
+        vector<int> ans;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            ans.push_back(mp[nums1[i]]);
+        }
+
+        return ans;
+    }
+};
