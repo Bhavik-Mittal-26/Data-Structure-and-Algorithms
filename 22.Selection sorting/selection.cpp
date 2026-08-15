4178a7fde4c61a8a93138929f7b9a0a542fd2436
Bhavik Mittal <26bmittal@gmail.com>
2026-08-15 23:40:56 +0530

    updated code

--- 22.Selection sorting/selection.cpp
+++ 22.Selection sorting/selection.cpp
@@ -1,33 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void selectionSort(int arr[], int n) {
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Selection Sort
     for(int i = 0; i < n - 1; i++) {
-     int minIndex = i;
-        for(int j = i + 1; j < n; j++) {
-            if(arr[j] < arr[minIndex]) {
-                minIndex = j;
+
+        for(int j = i; j < n; j++) {
+
+            if(arr[j] < arr[i]) {
+                swap(arr[i], arr[j]);
             }
         }
-        if(minIndex != i) {
-            swap(arr[i], arr[minIndex]);
-        }
     }
-}
 
-void printArray(int arr[], int n) {
+    // Print sorted array
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
-    cout << endl;
-}
-
-int main() {
-    int n = 5;
-    int arr[] = {3,4,1,2,5};
-
-    selectionSort(arr, n);
-    printArray(arr, n);
 
     return 0;
 }
